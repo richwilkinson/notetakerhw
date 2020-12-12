320395f8df98ce6d2a5b99d2da083bda975a750a
rich wilk <richardwilkinson@live.com>
2020-12-11 23:07:09 -0500

    added first route to notes.html

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
+//node modules
 const express = require("express");
-
+const path = require("path");
+const fs = require("fs");
+//calling express
 const app = express();
-
+//creating port for server
 const PORT = process.env.PORT || 8080;
 
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get("/notes", (req, res) => {
+    res.sendfile(path.join(__dirname, "/../public/notes.html"));
+})
 
 app.listen(PORT, () => console.log(`serve is good on port ${PORT}`));
