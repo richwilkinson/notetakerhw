4f545bea9d46229c324e04947723a12d22008735
rich wilk <richardwilkinson@live.com>
2020-12-12 20:29:29 -0500

    added first two api routes

--- server.js
+++ server.js
@@ -12,10 +12,18 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.get("/notes", (req, res) => {
-    res.sendfile(path.join(__dirname, "./public/notes.html"));
+    res.sendfile(path.join(__dirname, "/public/notes.html"));
 });
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"))
+    res.sendFile(path.join(__dirname, "/public/index.html"))
 });
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(_dirname, "/db/db.json"))
+});
+app.get("/api/notes/:id", (req, res) => {
+    let savedNotes = JSON.parse(fs,fs.readFileSync("./db/db.json", "utf-8"));
+    res.json(savedNotes[Number(req.params.id)]);
+});
+
 
 app.listen(PORT, () => console.log(`serve is good on port ${PORT}`));
