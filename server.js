5ea5783f14592c0c9457acb3e3e6ac7ae2543745
rich wilk <richardwilkinson@live.com>
2020-12-12 19:59:39 -0500

    added html routes

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.get("/notes", (req, res) => {
-    res.sendfile(path.join(__dirname, "/../public/notes.html"));
-})
+    res.sendfile(path.join(__dirname, "./public/notes.html"));
+});
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+});
 
 app.listen(PORT, () => console.log(`serve is good on port ${PORT}`));
